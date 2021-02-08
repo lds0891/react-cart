7a5c4c94cbaa9ef66c1a577eef082e7e9ce736d0
Dmitriy Logachev <dmitriy.logachev.adci@gmail.com>
2021-02-08 13:02:02 +0600

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React, { Component } from 'react';
+import Counter from './components/Counter/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Counter/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './Counter.css';
+
+class Counter extends Component {
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+          count: 0,
+        };
+    }
+
+    countUp = () => {
+        this.setState({
+          count: this.state.count + 1,
+        });
+    };
+
+    countDown = () => {
+        this.setState({
+          count: this.state.count - 1,
+        });
+    };
+
+    render(){
+        let { count } = this.state;
+
+        return(
+            <div className="counter">
+                <button
+                    className="counter__btn"
+                    onClick={this.countDown}
+                >
+                    -
+                </button>
+                <span className="counter__number">
+                    { count }
+                </span>
+                <button
+                    className="counter__btn"
+                    onClick={this.countUp}
+                >
+                    +
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
