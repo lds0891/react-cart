1f752758400a8319e82fc2677735efe10582497e
Dmitriy Logachev <dmitriy.logachev.adci@gmail.com>
2021-02-11 08:58:31 +0600

    Add switch route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from "./components/Header";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Bags from "./components/Bags";
 import 'normalize.css';
 import './App.css';
@@ -12,14 +12,14 @@ class App extends Component {
   render() {
 
     return (
-      <Router>
+      <Switch>
         <div className="App">
           <Header/>
           <Route exact path="/" component={CounterFunc} />
           <Route exact path="/bags" component={Bags} />
           <Route exact path="/shoes" component={BagsFunc} />
         </div>
-      </Router>
+      </Switch>
     );
   }
 }
