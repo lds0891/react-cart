1e3d219f9be84ea156cc78943eecaf81132d29fb
Dmitry Logachoyv <lds0891@gmail.com>
2021-03-05 12:02:52 +0600

    Fix bags array

--- src/back.js
+++ src/back.js
@@ -1,26 +1,45 @@
-import img from './assets/img/bag.png';
+import img from './assets/img/bags/bag.png';
+import img2 from './assets/img/bags/bag2.png';
+import img3 from './assets/img/bags/bag3.png';
+import img4 from './assets/img/bags/bag4.png';
+import img5 from './assets/img/bags/bag5.png';
+import img6 from './assets/img/bags/bag6.png';
+import img7 from './assets/img/bags/bag7.png';
+import img8 from './assets/img/bags/bag8.png';
+import img9 from './assets/img/bags/bag9.png';
+import img10 from './assets/img/bags/bag10.png';
+import img11 from './assets/img/bags/bag11.png';
+import img12 from './assets/img/bags/bag12.png';
+import img13 from './assets/img/bags/bag13.png';
+import img14 from './assets/img/bags/bag14.png';
+import img15 from './assets/img/bags/bag15.png';
+import img16 from './assets/img/bags/bag16.png';
+import img17 from './assets/img/bags/bag17.png';
+import img18 from './assets/img/bags/bag18.png';
+import img19 from './assets/img/bags/bag19.png';
+import img20 from './assets/img/bags/bag20.png';
 
 const bags = [
     {img: img, name: 'Bag', price: 100, currency:'$', id: 0, quantity: 1},
-    {img: img, name: 'Bag2', price: 790, currency:'$', id: 1, quantity: 1},
-    {img: img, name: 'Bag3', price: 10, currency:'$', id: 2, quantity: 1},
-    {img: img, name: 'Bag4', price: 20, currency:'$', id: 3, quantity: 1},
-    {img: img, name: 'Bag5', price: 200, currency:'$', id: 4, quantity: 1},
-    {img: img, name: 'Bag6', price: 120, currency:'$', id: 5, quantity: 1},
-    {img: img, name: 'Bag7', price: 313, currency:'$', id: 6, quantity: 1},
-    {img: img, name: 'Bag8', price: 155, currency:'$', id: 7, quantity: 1},
-    {img: img, name: 'Bag9', price: 170, currency:'$', id: 8, quantity: 1},
-    {img: img, name: 'Bag10', price: 153, currency:'$', id: 9, quantity: 1},
-    {img: img, name: 'Bag11', price: 112, currency:'$', id: 10, quantity: 1},
-    {img: img, name: 'Bag12', price: 52, currency:'$', id: 11, quantity: 1},
-    {img: img, name: 'Bag13', price: 30, currency:'$', id: 12, quantity: 1},
-    {img: img, name: 'Bag14', price: 270, currency:'$', id: 13, quantity: 1},
-    {img: img, name: 'Bag15', price: 188, currency:'$', id: 14, quantity: 1},
-    {img: img, name: 'Bag16', price: 599, currency:'$', id: 15, quantity: 1},
-    {img: img, name: 'Bag17', price: 10, currency:'$', id: 16, quantity: 1},
-    {img: img, name: 'Bag18', price: 25, currency:'$', id: 17, quantity: 1},
-    {img: img, name: 'Bag19', price: 300, currency:'$', id: 18, quantity: 1},
-    {img: img, name: 'Bag20', price: 1000, currency:'$', id: 19, quantity: 1},
+    {img: img2, name: 'Bag2', price: 790, currency:'$', id: 1, quantity: 1},
+    {img: img3, name: 'Bag3', price: 10, currency:'$', id: 2, quantity: 1},
+    {img: img4, name: 'Bag4', price: 20, currency:'$', id: 3, quantity: 1},
+    {img: img5, name: 'Bag5', price: 200, currency:'$', id: 4, quantity: 1},
+    {img: img6, name: 'Bag6', price: 120, currency:'$', id: 5, quantity: 1},
+    {img: img7, name: 'Bag7', price: 313, currency:'$', id: 6, quantity: 1},
+    {img: img8, name: 'Bag8', price: 155, currency:'$', id: 7, quantity: 1},
+    {img: img9, name: 'Bag9', price: 170, currency:'$', id: 8, quantity: 1},
+    {img: img10, name: 'Bag10', price: 153, currency:'$', id: 9, quantity: 1},
+    {img: img11, name: 'Bag11', price: 112, currency:'$', id: 10, quantity: 1},
+    {img: img12, name: 'Bag12', price: 52, currency:'$', id: 11, quantity: 1},
+    {img: img13, name: 'Bag13', price: 30, currency:'$', id: 12, quantity: 1},
+    {img: img14, name: 'Bag14', price: 270, currency:'$', id: 13, quantity: 1},
+    {img: img15, name: 'Bag15', price: 188, currency:'$', id: 14, quantity: 1},
+    {img: img16, name: 'Bag16', price: 599, currency:'$', id: 15, quantity: 1},
+    {img: img17, name: 'Bag17', price: 10, currency:'$', id: 16, quantity: 1},
+    {img: img18, name: 'Bag18', price: 25, currency:'$', id: 17, quantity: 1},
+    {img: img19, name: 'Bag19', price: 300, currency:'$', id: 18, quantity: 1},
+    {img: img20, name: 'Bag20', price: 1000, currency:'$', id: 19, quantity: 1},
 ]
 
 export default bags;
