cb2053ff8b0afbca70b471040e286773d12087b7
Dmitry Logachoyv <lds0891@gmail.com>
2021-03-09 15:08:11 +0600

    Modifying an array with products

--- src/back.js
+++ src/back.js
@@ -1,45 +1,127 @@
-import img from './assets/img/bags/bag.png';
-import img2 from './assets/img/bags/bag2.png';
-import img3 from './assets/img/bags/bag3.png';
-import img4 from './assets/img/bags/bag4.png';
-import img5 from './assets/img/bags/bag5.png';
-import img6 from './assets/img/bags/bag6.png';
-import img7 from './assets/img/bags/bag7.png';
-import img8 from './assets/img/bags/bag8.png';
-import img9 from './assets/img/bags/bag9.png';
-import img10 from './assets/img/bags/bag10.png';
-import img11 from './assets/img/bags/bag11.png';
-import img12 from './assets/img/bags/bag12.png';
-import img13 from './assets/img/bags/bag13.png';
-import img14 from './assets/img/bags/bag14.png';
-import img15 from './assets/img/bags/bag15.png';
-import img16 from './assets/img/bags/bag16.png';
-import img17 from './assets/img/bags/bag17.png';
-import img18 from './assets/img/bags/bag18.png';
-import img19 from './assets/img/bags/bag19.png';
-import img20 from './assets/img/bags/bag20.png';
+import bag from './assets/img/bags/bag.png';
+import bag2 from './assets/img/bags/bag2.png';
+import bag3 from './assets/img/bags/bag3.png';
+import bag4 from './assets/img/bags/bag4.png';
+import bag5 from './assets/img/bags/bag5.png';
+import bag6 from './assets/img/bags/bag6.png';
+import bag7 from './assets/img/bags/bag7.png';
+import bag8 from './assets/img/bags/bag8.png';
+import bag9 from './assets/img/bags/bag9.png';
+import bag10 from './assets/img/bags/bag10.png';
+import bag11 from './assets/img/bags/bag11.png';
+import bag12 from './assets/img/bags/bag12.png';
+import bag13 from './assets/img/bags/bag13.png';
+import bag14 from './assets/img/bags/bag14.png';
+import bag15 from './assets/img/bags/bag15.png';
+import bag16 from './assets/img/bags/bag16.png';
+import bag17 from './assets/img/bags/bag17.png';
+import bag18 from './assets/img/bags/bag18.png';
+import bag19 from './assets/img/bags/bag19.png';
+import bag20 from './assets/img/bags/bag20.png';
+import hats from './assets/img/hats/hats.png';
+import hats2 from './assets/img/hats/hats2.png';
+import hats3 from './assets/img/hats/hats3.png';
+import hats4 from './assets/img/hats/hats4.png';
+import hats5 from './assets/img/hats/hats5.png';
+import hats6 from './assets/img/hats/hats6.png';
+import hats7 from './assets/img/hats/hats7.png';
+import hats8 from './assets/img/hats/hats8.png';
+import hats9 from './assets/img/hats/hats9.png';
+import hats10 from './assets/img/hats/hats10.png';
+import hats11 from './assets/img/hats/hats11.png';
+import hats12 from './assets/img/hats/hats12.png';
+import hats13 from './assets/img/hats/hats13.png';
+import hats14 from './assets/img/hats/hats14.png';
+import hats15 from './assets/img/hats/hats15.png';
+import hats16 from './assets/img/hats/hats16.png';
+import hats17 from './assets/img/hats/hats17.png';
+import hats18 from './assets/img/hats/hats18.png';
+import hats19 from './assets/img/hats/hats19.png';
+import hats20 from './assets/img/hats/hats20.png';
+import shoes from './assets/img/shoes/shoes.png';
+import shoes2 from './assets/img/shoes/shoes2.png';
+import shoes3 from './assets/img/shoes/shoes3.png';
+import shoes4 from './assets/img/shoes/shoes4.png';
+import shoes5 from './assets/img/shoes/shoes5.png';
+import shoes6 from './assets/img/shoes/shoes6.png';
+import shoes7 from './assets/img/shoes/shoes7.png';
+import shoes8 from './assets/img/shoes/shoes8.png';
+import shoes9 from './assets/img/shoes/shoes9.png';
+import shoes10 from './assets/img/shoes/shoes10.png';
+import shoes11 from './assets/img/shoes/shoes11.png';
+import shoes12 from './assets/img/shoes/shoes12.png';
+import shoes13 from './assets/img/shoes/shoes13.png';
+import shoes14 from './assets/img/shoes/shoes14.png';
+import shoes15 from './assets/img/shoes/shoes15.png';
+import shoes16 from './assets/img/shoes/shoes16.png';
+import shoes17 from './assets/img/shoes/shoes17.png';
+import shoes18 from './assets/img/shoes/shoes18.png';
+import shoes19 from './assets/img/shoes/shoes19.png';
+import shoes20 from './assets/img/shoes/shoes20.png';
 
-const bags = [
-    {img: img, name: 'Bag', price: 100, currency:'$', id: 0, quantity: 1},
-    {img: img2, name: 'Bag2', price: 790, currency:'$', id: 1, quantity: 1},
-    {img: img3, name: 'Bag3', price: 10, currency:'$', id: 2, quantity: 1},
-    {img: img4, name: 'Bag4', price: 20, currency:'$', id: 3, quantity: 1},
-    {img: img5, name: 'Bag5', price: 200, currency:'$', id: 4, quantity: 1},
-    {img: img6, name: 'Bag6', price: 120, currency:'$', id: 5, quantity: 1},
-    {img: img7, name: 'Bag7', price: 313, currency:'$', id: 6, quantity: 1},
-    {img: img8, name: 'Bag8', price: 155, currency:'$', id: 7, quantity: 1},
-    {img: img9, name: 'Bag9', price: 170, currency:'$', id: 8, quantity: 1},
-    {img: img10, name: 'Bag10', price: 153, currency:'$', id: 9, quantity: 1},
-    {img: img11, name: 'Bag11', price: 112, currency:'$', id: 10, quantity: 1},
-    {img: img12, name: 'Bag12', price: 52, currency:'$', id: 11, quantity: 1},
-    {img: img13, name: 'Bag13', price: 30, currency:'$', id: 12, quantity: 1},
-    {img: img14, name: 'Bag14', price: 270, currency:'$', id: 13, quantity: 1},
-    {img: img15, name: 'Bag15', price: 188, currency:'$', id: 14, quantity: 1},
-    {img: img16, name: 'Bag16', price: 599, currency:'$', id: 15, quantity: 1},
-    {img: img17, name: 'Bag17', price: 10, currency:'$', id: 16, quantity: 1},
-    {img: img18, name: 'Bag18', price: 25, currency:'$', id: 17, quantity: 1},
-    {img: img19, name: 'Bag19', price: 300, currency:'$', id: 18, quantity: 1},
-    {img: img20, name: 'Bag20', price: 1000, currency:'$', id: 19, quantity: 1},
+const back = [
+    {type: 'bag', img: bag, name: 'Bag', price: 100, currency:'$', id: 0, quantity: 11},
+    {type: 'bag', img: bag2, name: 'Bag2', price: 790, currency:'$', id: 1, quantity: 5},
+    {type: 'bag', img: bag3, name: 'Bag3', price: 10, currency:'$', id: 2, quantity: 8},
+    {type: 'bag', img: bag4, name: 'Bag4', price: 20, currency:'$', id: 3, quantity: 21},
+    {type: 'bag', img: bag5, name: 'Bag5', price: 200, currency:'$', id: 4, quantity: 71},
+    {type: 'bag', img: bag6, name: 'Bag6', price: 120, currency:'$', id: 5, quantity: 6},
+    {type: 'bag', img: bag7, name: 'Bag7', price: 313, currency:'$', id: 6, quantity: 3},
+    {type: 'bag', img: bag8, name: 'Bag8', price: 155, currency:'$', id: 7, quantity: 8},
+    {type: 'bag', img: bag9, name: 'Bag9', price: 170, currency:'$', id: 8, quantity: 4},
+    {type: 'bag', img: bag10, name: 'Bag10', price: 153, currency:'$', id: 9, quantity: 9},
+    {type: 'bag', img: bag11, name: 'Bag11', price: 112, currency:'$', id: 10, quantity: 3},
+    {type: 'bag', img: bag12, name: 'Bag12', price: 52, currency:'$', id: 11, quantity: 8},
+    {type: 'bag', img: bag13, name: 'Bag13', price: 30, currency:'$', id: 12, quantity: 25},
+    {type: 'bag', img: bag14, name: 'Bag14', price: 270, currency:'$', id: 13, quantity: 36},
+    {type: 'bag', img: bag15, name: 'Bag15', price: 188, currency:'$', id: 14, quantity: 44},
+    {type: 'bag', img: bag16, name: 'Bag16', price: 599, currency:'$', id: 15, quantity: 20},
+    {type: 'bag', img: bag17, name: 'Bag17', price: 10, currency:'$', id: 16, quantity: 30},
+    {type: 'bag', img: bag18, name: 'Bag18', price: 25, currency:'$', id: 17, quantity: 8},
+    {type: 'bag', img: bag19, name: 'Bag19', price: 300, currency:'$', id: 18, quantity: 9},
+    {type: 'bag', img: bag20, name: 'Bag20', price: 1000, currency:'$', id: 19, quantity: 11},
+
+    {type: 'hats', img: hats, name: 'Hats', price: 99, currency:'$', id: 100, quantity: 11},
+    {type: 'hats', img: hats2, name: 'Hats2', price: 540, currency:'$', id: 101, quantity: 12},
+    {type: 'hats', img: hats3, name: 'Hats3', price: 50, currency:'$', id: 102, quantity: 15},
+    {type: 'hats', img: hats4, name: 'Hats4', price: 20, currency:'$', id: 103, quantity: 17},
+    {type: 'hats', img: hats5, name: 'Hats5', price: 220, currency:'$', id: 104, quantity: 21},
+    {type: 'hats', img: hats6, name: 'Hats6', price: 300, currency:'$', id: 105, quantity: 8},
+    {type: 'hats', img: hats7, name: 'Hats7', price: 870, currency:'$', id: 106, quantity: 3},
+    {type: 'hats', img: hats8, name: 'Hats8', price: 155, currency:'$', id: 107, quantity: 7},
+    {type: 'hats', img: hats9, name: 'Hats9', price: 50, currency:'$', id: 108, quantity: 22},
+    {type: 'hats', img: hats10, name: 'Hats10', price: 507, currency:'$', id: 109, quantity: 6},
+    {type: 'hats', img: hats11, name: 'Hats11', price: 800, currency:'$', id: 110, quantity: 77},
+    {type: 'hats', img: hats12, name: 'Hats12', price: 52, currency:'$', id: 111, quantity: 14},
+    {type: 'hats', img: hats13, name: 'Hats13', price: 30, currency:'$', id: 112, quantity: 11},
+    {type: 'hats', img: hats14, name: 'Hats14', price: 270, currency:'$', id: 113, quantity: 6},
+    {type: 'hats', img: hats15, name: 'Hats15', price: 987, currency:'$', id: 114, quantity: 34},
+    {type: 'hats', img: hats16, name: 'Hats16', price: 100, currency:'$', id: 115, quantity: 4},
+    {type: 'hats', img: hats17, name: 'Hats17', price: 30, currency:'$', id: 116, quantity: 7},
+    {type: 'hats', img: hats18, name: 'Hats18', price: 70, currency:'$', id: 117, quantity: 24},
+    {type: 'hats', img: hats19, name: 'Hats19', price: 400, currency:'$', id: 118, quantity: 31},
+    {type: 'hats', img: hats20, name: 'Hats20', price: 600, currency:'$', id: 119, quantity: 9},
+
+    {type: 'hats', img: shoes, name: 'Shoes', price: 150, currency:'$', id: 200, quantity: 5},
+    {type: 'hats', img: shoes2, name: 'Shoes2', price: 579, currency:'$', id: 201, quantity: 6},
+    {type: 'hats', img: shoes3, name: 'Shoes3', price: 122, currency:'$', id: 202, quantity: 2},
+    {type: 'hats', img: shoes4, name: 'Shoes4', price: 33, currency:'$', id: 203, quantity: 14},
+    {type: 'hats', img: shoes5, name: 'Shoes5', price: 500, currency:'$', id: 204, quantity: 11},
+    {type: 'hats', img: shoes6, name: 'Shoes6', price: 781, currency:'$', id: 205, quantity: 19},
+    {type: 'hats', img: shoes7, name: 'Shoes7', price: 88, currency:'$', id: 206, quantity: 17},
+    {type: 'hats', img: shoes8, name: 'Shoes8', price: 255, currency:'$', id: 207, quantity: 20},
+    {type: 'hats', img: shoes9, name: 'Shoes9', price: 101, currency:'$', id: 208, quantity: 60},
+    {type: 'hats', img: shoes10, name: 'Shoes10', price: 99, currency:'$', id: 209, quantity: 41},
+    {type: 'hats', img: shoes11, name: 'Shoes11', price: 210, currency:'$', id: 210, quantity: 6},
+    {type: 'hats', img: shoes12, name: 'Shoes12', price: 99, currency:'$', id: 211, quantity: 41},
+    {type: 'hats', img: shoes13, name: 'Shoes13', price: 30, currency:'$', id: 212, quantity: 20},
+    {type: 'hats', img: shoes14, name: 'Shoes14', price: 300, currency:'$', id: 213, quantity: 33},
+    {type: 'hats', img: shoes15, name: 'Shoes15', price: 120, currency:'$', id: 214, quantity: 41},
+    {type: 'hats', img: shoes16, name: 'Shoes16', price: 455, currency:'$', id: 215, quantity: 22},
+    {type: 'hats', img: shoes17, name: 'Shoes17', price: 100, currency:'$', id: 216, quantity: 24},
+    {type: 'hats', img: shoes18, name: 'Shoes18', price: 400, currency:'$', id: 217, quantity: 5},
+    {type: 'hats', img: shoes19, name: 'Shoes19', price: 1200, currency:'$', id: 218, quantity: 6},
+    {type: 'hats', img: shoes20, name: 'Shoes20', price: 999, currency:'$', id: 219, quantity: 8},
 ]
 
-export default bags;
+export default back;
